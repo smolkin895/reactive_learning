214827cfaab671b3db50c14ae61e4967f61e640d
smolkin895 <smol140791@ya.ru>
2024-10-24 23:01:17 +0300

    lesson_3

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 import {test_func} from '../test/test';
 
 test_func('LoOodash')
+
+document.body.innerHTML = `<div>
+                                <p>Корабль пошел!!!</p>
+                            </div>`;
+
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,33 +1,23 @@
 import path from "path";
 import webpack from "webpack";
-import HTMLWebpackPlugin from "html-webpack-plugin";
+import { buildWebpackConfig } from "./config/build/buildWebpackConfig";
+import { BuildDev, BuildPaths } from "./config/build/types/config";
 
-const config: webpack.Configuration ={
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src', 'index.ts'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
-        clean: true,
-    },
-    module: {
-        rules: [
-          {
-            test: /\.tsx?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-        ],
-      },
-      resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-      },
-    plugins:[
-        new HTMLWebpackPlugin({
-            template: path.resolve(__dirname, 'public', 'index.html') 
-        }),
-        new webpack.ProgressPlugin(),
-    ]
+
+
+export default (env: BuildDev) => {
+const  mode = env.mode || 'development';
+const isDev = mode === 'development';
+const PORT = env.port || 3000;
+const paths: BuildPaths = {
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  build: path.resolve(__dirname, 'dist'),
+  html:  path.resolve(__dirname, 'public', 'index.html') 
 }
-
-export default config;
+  return buildWebpackConfig({
+    mode,
+    paths,
+    isDev,
+    port: PORT,
+  });
+};
